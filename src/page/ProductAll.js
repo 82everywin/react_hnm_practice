f6add8f19466081f1e3c69dcd5c222a59e31dfc6
hyeonseung <82everywin@gmail.com>
2024-03-29 00:51:15 +0900

    6-9강

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from 'react'
+import { Col, Container, Row} from 'react-bootstrap';
 import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
 
     const [productList, setProductList] = useState([]);
     const getProducts = async() => {
-        let url = "http://localhost:5000/products"
-        let response = await fetch(url)
+        let url = "http://localhost:5000/products";
+        let response = await fetch(url);
         let data = await response.json();
+        setProductList(data);
         console.log(data)
     }
+
     useEffect(()=>{
-        getProducts()
+        getProducts();;
     },[])
+
+
   return (
     <div>
-        
-      <h1> 전체 상품 페이지 </h1>
-        <ProductCard/>
+        <Container>
+          <Row className='Card' >
+           {productList.map((item)=>(
+                <Col lg={3} className="col">
+                  <ProductCard item={item}/>
+                </Col>
+              )) }
+          </Row>
+        </Container>
     </div>
-  )
-}
+  );
+};
 
 export default ProductAll
