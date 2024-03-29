ec8b5701e5e7ceeb6195f226a6afe72bc73b430c
82everywin <82everywin@gmail.com>
2024-03-30 04:12:59 +0900

    modify

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -11,7 +11,7 @@ const ProductDetail = () => {
 
 
   const getProductDetail = async() => {
-    let url = `https://my-json-server.typicode.com/82everywin/react_hnm_practice/tproducts/${id} `
+    let url = `https://my-json-server.typicode.com/82everywin/react_hnm_practice/products/${id} `
     let response = await fetch(url)
     let data = await response.json();
     console.log(data);
